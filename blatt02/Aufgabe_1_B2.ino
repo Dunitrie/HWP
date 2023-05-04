97d2e0be4eb5908cc0e7c323dc27c01abde94ddf
Dun <danielburkhardt@freenet.de>
2023-05-04 22:56:50 +0200

    New version

--- blatt02/Aufgabe_1_B2.ino
+++ blatt02/Aufgabe_1_B2.ino
@@ -1,6 +1,3 @@
-#define MOTOR_PIN1 D1
-#define MOTOR_PIN2 D2
-
 void setup() {
 pinMode(D1, OUTPUT);
 digitalWrite(D1, LOW);
@@ -13,52 +10,52 @@ digitalWrite(D6, LOW);
 
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-turn(0, 1000, 100);
-// drive(0, 1000, 100);
-// setMotor(0, true, 50);
-delay(5000);
+  // Turn the device
+  turn(0, 1000, 100);
+  // drive(0, 1000, 100);
+  // setMotor(0, true, 50);
+  delay(5000);
 }
 
 void setMotor(bool motor, bool forward, uint16_t speed)  {
-if (motor) {
+  if (motor) {
+    if (forward) {
+      digitalWrite(D1, 0);
+      analogWrite(D2, speed);
+    }
+    else {
+      digitalWrite(D2, 0);
+      analogWrite(D1, speed);
+    }
+  }
+  else{
+    if (forward) {
+    digitalWrite(D5, 0);
+    analogWrite(D6, speed);
+    }
+    else {
+      digitalWrite(D6, 0);
+      analogWrite(D5, speed);
+    }
+  }
+}
 
+void drive(bool direction, uint16_t time, uint16_t speed) {
+  // If direction==0: Drive backwards, otherwise forward with speed speed
+  unsigned long starting_time = millis();
 
-if (forward) {
-  digitalWrite(D1, 0);
-  analogWrite(D2, speed);
+  while (millis() - starting_time < time) {
+    setMotor(0, direction, speed);
+    setMotor(1, direction, speed);
+  }
+  setMotor(0, direction, 0);
+  setMotor(1, direction, 0);
 }
-else {
-digitalWrite(D2, 0);
-analogWrite(D1, speed);
-}
-}
-else{
-  if (forward) {
-  digitalWrite(D5, 0);
-  analogWrite(D6, speed);
-}
-else {
-digitalWrite(D6, 0);
-analogWrite(D5, speed);
-}
-}
-}
- void drive(bool direction, uint16_t time, uint16_t speed) {
-   unsigned long starting_time = millis();
 
-   while (millis() - starting_time < time) {
-     setMotor(0, direction, speed);
-     setMotor(1, direction, speed);
-   }
-   setMotor(0, direction, 0);
-   setMotor(1, direction, 0);
- }
+void turn(bool direction, uint16_t time, uint16_t speed){
+  unsigned long starting_time = millis();
 
- void turn(bool direction, uint16_t time, uint16_t speed){
-    unsigned long starting_time = millis();
-
-   while (millis() - starting_time < time){
+  while (millis() - starting_time < time){
     if (direction) {
       setMotor(0, direction, speed);
       setMotor(1, !direction, speed);
@@ -67,8 +64,8 @@ analogWrite(D5, speed);
       setMotor(1, direction, speed);
       setMotor(0, !direction, speed);
     }
-   }
-   setMotor(0, direction, 0);
-   setMotor(1, direction, 0);
- }
+  }
+  setMotor(0, direction, 0);
+  setMotor(1, direction, 0);
+}
 
