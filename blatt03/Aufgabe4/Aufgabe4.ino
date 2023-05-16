e2a1101e92b8cd81aec5b72d3b56861996ca2159
Dun <danielburkhardt@freenet.de>
2023-05-16 20:45:50 +0200

    really added Aufgabe4

--- blatt03/Aufgabe4/Aufgabe4.ino
+++ blatt03/Aufgabe4/Aufgabe4.ino
@@ -4,38 +4,47 @@ void setup() {
   // put your setup code here, to run once:
   DDRB |= (1<<PB2);
   PORTB &= ~(1<<PB2);
-  setTimer1Freq();
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-ISR(TIMER1_COMPA_vect) {
-//   PINB |= (1 << 4);
-}
-
-void setTimer1Freq(){
-  cli();
   //reset control registers
   TCCR1A = 0;
   TCCR1B = 0;
-  //TIMSK1 = 0;
-
-  //prescaler: 64
-  TCCR1B |= (1 << CS11);
-  TCCR1B |= (1 << CS10);
-
+  TIMSK1 = 0;
+  // We want to toggle OCR1A on pin 10
+  // Timer Counter Control Register 1 
+  TCCR1A |= (1 << COM1B0);
   //CTC-MODE
   TCCR1B |= (1 << WGM12);
+  //prescaler: 64
+  TCCR1B |= (1 << CS12)| (1 << CS10);
 
-  //Set output compare register A
-  OCR1A = 60;
-
-  //enable interrupt
-  TCCR1A |= (1<<COM1A0);
   
+  Serial.begin(19200);
+
+}
+
+void loop() {
+  setTimer1Freq(3000);
+  delay(2000);
+  setTimer1Freq(1000);
+  delay(2000);
+}
+
+
+void setTimer1Freq(uint16_t frequency){
+
+  // TODO: Here we need to have a prescaler. Right now, we just use 64 (which might even be incorrect)
+  OCR1A = return_OCR1A(frequency, 64);
+  Serial.print("OCR1A: ");
+  Serial.println(OCR1A);
+  //enable interrupt
   // TIMSK1 |= (1 << OCIE1A);
   //enable all interrputs
-  sei();
+  // setTimer1Freq();
+}
+
+long return_OCR1A(uint16_t frequency, uint16_t prescaler){
+  // F_CPU is the clock frequency
+  // Derived from p.99 datasheet
+  long val = F_CPU / 2 / prescaler / frequency - 1;
+  ///Serial.println(val);
+  return val;
 }
