2323f13eaafb360a0a11c3ed6256f7cad360d9bc
Dun <danielburkhardt@freenet.de>
2023-05-31 14:51:24 +0200

    added no. 2 initial commit

--- blatt05/Aufgabe1CounterTemplate_restored/vhdl/analog_to_buttons.vhd
+++ blatt05/Aufgabe1CounterTemplate_restored/vhdl/analog_to_buttons.vhd
@@ -4,11 +4,13 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
 
 entity analog_to_buttons is port(
 	analogIn : in std_logic_vector(11 downto 0);
-	
+	-- Wir hatten in der Vorlage button1 und button3, obwohl im Blatt 4 und 5 stand. 
+	-- Wir verwenden 4 für 1 und 5 für 3;
 	button1pressed : out std_logic;
 	button3pressed : out std_logic
 );
@@ -17,7 +19,14 @@ end analog_to_buttons;
 
 architecture behavior of analog_to_buttons is
 begin
+	process(analogIn)
+	begin 
+		button1pressed <= '0'; -- Initialize output signals
+		button3pressed <= '0';
 
-	-- TODO!
+		if (unsigned(analogIn) <= 2800 and unsigned(analogIn) <= 2040) then button1pressed <= '1';
+		elsif (unsigned(analogIn) <= 3600 and unsigned(analogIn) >= 2840) then button3pressed <= '1';
+		end if;
+	end process; 
 
 end behavior;
