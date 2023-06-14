637eb1bd6140f05513cd34e631861661fa21ec69
Dun <danielburkhardt@freenet.de>
2023-06-14 17:28:26 +0200

    Does compile

--- blatt06/ultrasound_controller_restored/vhdl/ultrasound_controller.vhd
+++ blatt06/ultrasound_controller_restored/vhdl/ultrasound_controller.vhd
@@ -22,47 +22,141 @@ end ultrasound_controller;
 
 
 architecture behavior of ultrasound_controller is
-	signal clk_counter: std_logic_vector(11 downto 0);
-	signal distance_in: std_logic_vector(7 downto 0);
+	constant maxCounter : std_logic_vector(4 downto 0) := "11001";
+	constant maxCounter_ns : std_logic_vector(9 downto 0) := "1111101000";
+	
+	signal no_response_flag : std_logic := '0';
+
+	signal counter: std_logic_vector(4 downto 0);
+	
+	signal distance_counter: std_logic_vector(5 downto 0) := "000000";
+		
+	signal ns_counter: std_logic_vector(9 downto 0) := "0000000000";
+	signal us_counter: std_logic_vector(14 downto 0) := "000000000000000";
+	signal ms_counter: std_logic_vector(2 downto 0) := "000";
 	
 begin
 
-process(clk_50, enable, trigger)
-	begin
-	finished <= '1';
 
-	-- TODO
-	-- Repeat the following:
-	-- 	trigger to output 
-	-- 	wait for 10mus
-	-- 	trigger to input
+-- We implement multiple clocks for ns, us and ms
+ns_clock_counter: process(clk_50)
+begin
+	if rising_edge(clk_50) then
+		if counter = maxCounter then -- Counts up to 1k
+			counter <= (others => '0');
+			ns_counter <= std_logic_vector(unsigned(ns_counter) + 1);
+		else
+			counter <= std_logic_vector(unsigned(counter) + 1);
+		end if;
+	end if;
+
+end process;
+
+
+
+us_clock_counter: process(clk_50)
+begin
+	if rising_edge(clk_50) then
+		if ns_counter = maxCounter_ns then
+			ns_counter <= (others => '0');
+			us_counter <= std_logic_vector(unsigned(us_counter) + 1);
+		--else
+		end if;
+		
+		-- Reset after certain time 
+		if us_counter = "111111111111111" then
+			us_counter <= (others => '0');
+		end if;
+	end if;
+end process;
+
+ms_clock_counter: process(clk_50)
+begin
+	if rising_edge(clk_50) then
+		if us_counter = "000001111101000" then -- 1000 (decimal)
+			us_counter <= (others => '0');
+			ms_counter <= std_logic_vector(unsigned(ms_counter) + 1);
+		end if;
+		
+		-- Reset after certain time (Our max time is 3 ms, so we simply reset after 7 ms)
+		if ms_counter = "111" then
+			ms_counter <= (others => '0');
+		end if;
+	end if;
+end process;
+
+
+
+process(enable, trigger)
+	-- We implement these counters because we received errors, when our loops were too long
+	variable inner_loop_counter : integer := 0;
+	variable outer_loop_counter : integer := 0;
+	
+
+	begin
+	finished <= '1'; -- This value is supposed to be 0 exactly, if we're not in the loop
+	
+	-- This works like follows:
+	-- 	set trigger to output 
+	-- 	wait for 10mus 
+	-- 	set trigger to input
 	-- 	if rising_flank(trigger):
 	--		measure till falling_flank(trigger)
 	--			Somehow divide by 58
 	-- 	put to LCD
-	if rising_edge(enable) then
+
+	if rising_edge(enable) then	-- Wait for enabling
 		finished <= '0';
-		trigger <= '1';
-		wait for 10 us;
+		
+		
+		trigger <= '1'; -- send signal
+		
+		us_counter <= (others => '0'); --reset counters
+		ns_counter <= (others => '0');
+		while not us_counter = "0000000001010" loop -- Wait 10 us
+		end loop;		
 		trigger <= '0';
-		trigger <= 'Z';
-
-		wait until rising_edge(trigger);
-		--wait until falling_edge(trigger);
-		-- measure time:
-		-- with a 50MHz clock, we count the ns by counting to 50 with our rising-edge counter
-		-- then to divide by 58, we simply let our clk count to binary(50*58)=101101010100
-		clk_counter <= (others => '0');
-		while (trigger=1) loop
-			wait until rising_edge(clk_50);
-			clk_counter <= std_logic_vector(unsigned(clk_counter) + 1);
-			if clk_counter = "101101010100" then
-				distance_in <= std_logic_vector(unsigned(distance_in) + 1);
-				distance <= distance_in;
-				clk_counter <= (others => '0');
-			end if;	
+		trigger <= 'Z'; -- Disable trigger for FPGA to only be sensible to the sensor
+		
+		
+		us_counter <= (others => '0'); -- Reset all counters
+		ns_counter <= (others => '0');
+		ms_counter <= (others => '0');
+		-- If we waited too long, we put up a no-response-flag and return 0 as distance
+		while trigger='0' and no_response_flag = '0' and outer_loop_counter < 100 loop
+			inner_loop_counter := 0;
+			while trigger='0' and no_response_flag = '0' loop -- We split in inner and outer loop because 
+																			  -- we can't have too big loops
+				if ms_counter = "011" then -- if we waited for 3 miliseconds, we set the flag
+					no_response_flag <= '1';
+				end if;
+				inner_loop_counter := inner_loop_counter + 1;
+				
+				if inner_loop_counter >= 9999 then
+					exit;
+				end if;
+				
+			end loop;
+			outer_loop_counter := outer_loop_counter + 1;
 		end loop;
+		
+		
+		if no_response_flag = '0' then 
+			-- Measure distance
+			-- Since (if everything works) our loop from before breaks, if trigger = 1, this while looping 
+			-- is equivalent to measuring the time, when trigger is set to 1
+			ns_counter <= (others => '0');
+			while trigger='1' loop
+				if ns_counter = "0000111010" then -- Count to 58 (instead of dividing by 58)
+					distance_counter <= std_logic_vector(unsigned(distance_counter) + 1);
+					ns_counter <= (others => '0');
+				end if;
+			end loop;
+			distance <= distance_counter;
+		else 
+			distance <= (others => '0'); -- If we didn't find anything we return distance 0
+		end if;
+		no_response_flag <= '0';
 	end if;
-
 	end process;
 end behavior;
